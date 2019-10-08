5db1b91133369b146f438499a0e3bb112d163a7c
zekses <enmarantispam@gmail.com>
2019-10-08 17:54:44 +0300

    integrated circuitlord's camera

--- Assets/MikuEditor/Scripts/ThirdParty/MoveCamera.cs
+++ Assets/MikuEditor/Scripts/ThirdParty/MoveCamera.cs
@@ -7,24 +7,15 @@ using MiKu.NET;
 
 public class MoveCamera : MonoBehaviour 
 {
-	//
-	// VARIABLES
-	//
 	
 	public float turnSpeed = 4.0f;		// Speed of camera turning when mouse moves in along an axis
 	public float panSpeed = 4.0f;		// Speed of the camera when being panned
-	public float zoomSpeed = 4.0f;		// Speed of the camera going back and forth
-	
-	private Vector3 mouseOrigin;	// Position of cursor when mouse dragging starts
-	private bool isPanning;		// Is the camera being panned?
-	private bool isRotating;	// Is the camera being rotated?
-	
-    private Camera attachedCamera;
-    private bool isCTRLDown = false;
 
-	private bool isSHIFTDown = false;
+	private Vector2 mouseOrigin;	// Position of cursor when mouse dragging starts
 
-    private Vector3 defaultPosition;
+	private Camera attachedCamera;
+
+	private Vector3 defaultPosition;
     private Quaternion defaulRotation; 
 	private float defaultFOV;
 
@@ -42,105 +33,64 @@ public class MoveCamera : MonoBehaviour
         }
         
     }
-
-	void OnApplicationFocus(bool hasFocus)
-	{
-		if(hasFocus) {
-			isCTRLDown = false;
-			isSHIFTDown = false;
-		} 
+    
+    private void PanCamera(Vector3 direction) {
+		transform.position += Time.deltaTime * panSpeed * 8f * direction;
 	}
 	
 	void LateUpdate() 
 	{
 		if(!gameObject.activeSelf || !gameObject.activeInHierarchy) return;
 
-        if(Input.GetKeyDown(KeyCode.LeftAlt) || Input.GetKeyDown(KeyCode.RightAlt) 
-			|| Input.GetKeyDown(KeyCode.RightControl) || Input.GetKeyDown(KeyCode.LeftControl))
-        {
-            isCTRLDown = true;
-        }
-
-        if(Input.GetKeyUp(KeyCode.LeftAlt) || Input.GetKeyUp(KeyCode.RightAlt)
-			|| Input.GetKeyUp(KeyCode.RightControl) || Input.GetKeyUp(KeyCode.LeftControl)) {
-            isCTRLDown = false;
-        }
-
-		if(Input.GetKeyDown(KeyCode.LeftShift) || Input.GetKeyDown(KeyCode.RightShift)) {
-			isSHIFTDown = true;
+		Vector3 curPos = transform.position;
+		
+		if (Input.GetKey(KeyCode.A)) {
+			PanCamera(-transform.right);
+		}
+		
+		if (Input.GetKey(KeyCode.D)) {
+			PanCamera(transform.right);
+		}
+		
+		if (Input.GetKey(KeyCode.W)) {
+			PanCamera(transform.forward);
+		}
+		
+		if (Input.GetKey(KeyCode.S)) {
+			PanCamera(-transform.forward);
+		}
+		
+		if (Input.GetKey(KeyCode.Q)) {
+			PanCamera(-transform.up);
+		}
+		
+		if (Input.GetKey(KeyCode.E)) {
+			PanCamera(transform.up);
 		}
 
-		if(Input.GetKeyUp(KeyCode.LeftShift) || Input.GetKeyUp(KeyCode.RightShift)) {
-			isSHIFTDown = false;
+		//Start rotating the camera.
+		if (Input.GetMouseButtonDown(1)) {
+			mouseOrigin = Input.mousePosition;
 		}
 
-        if( (Input.GetKeyDown(KeyCode.Alpha8) || Input.GetKeyDown(KeyCode.Keypad8)) && isCTRLDown ) {
+		if (Input.GetMouseButton(1)) {
+			Vector2 curMousePos = Input.mousePosition;
+
+			float rightTurnSpeed = (curMousePos.y - mouseOrigin.y) * turnSpeed * Time.deltaTime;
+			float upTurnSpeed = (curMousePos.x - mouseOrigin.x) * turnSpeed * Time.deltaTime;
+			
+			transform.Rotate(Vector3.right * -rightTurnSpeed, Space.Self);
+			transform.Rotate(Vector3.up * upTurnSpeed, Space.World);
+
+			mouseOrigin = curMousePos;
+		}
+
+
+		if( (Input.GetKeyDown(KeyCode.Alpha8) || Input.GetKeyDown(KeyCode.Keypad8)) && Input.GetKey("Input Modifier1")) {
             ResetCamera();
             Miku_DialogManager.ShowDialog(Miku_DialogManager.DialogType.Info, "Camera position was reset");
         }
-
-		// Get the right mouse button
-		if(Input.GetMouseButtonDown(1))
-		{
-			// Get mouse origin
-			mouseOrigin = Input.mousePosition;
-            isRotating = true;
-		}
 		
-		// Get the middle mouse button
-		/* if(Input.GetMouseButtonDown(0) && isCTRLDown)
-		{
-			// Get mouse origin
-			mouseOrigin = Input.mousePosition;
-            isPanning = true;            
-		} */
-
-		float horizontalPanning = Input.GetAxis("Horizontal Free Camera");
-		float verticalPanning = Input.GetAxis("Vertical Free Camera");
-
-		if( (horizontalPanning != 0 || verticalPanning !=0) && !Track.PromtWindowOpen && !isCTRLDown && !isSHIFTDown) {
-			isPanning = true;		
-		} else {
-			isPanning = false;
-		}
-
-		/* if (Input.GetAxis("Mouse ScrollWheel") > 0f) // forward
-		{
-			DoZoom( -zoomSpeed );
-		} 
-		else if (Input.GetAxis("Mouse ScrollWheel") < 0f) // backwards
-		{
-			DoZoom( zoomSpeed );
-		} */
-		
-		// Disable movements on button release
-		if (!Input.GetMouseButton(1)) { isRotating = false; }
-		//if (!Input.GetMouseButton(0)) { isPanning = false; }
-		
-		// Rotate camera along X and Y axis
-		if (isRotating)
-		{
-            Vector3 pos = attachedCamera.ScreenToViewportPoint(Input.mousePosition - mouseOrigin);
-
-			transform.RotateAround(transform.position, transform.right, -pos.y * turnSpeed);
-			transform.RotateAround(transform.position, Vector3.up, pos.x * turnSpeed);
-		}
-		
-		// Move the camera on it's XY plane
-		if (isPanning)
-		{
-            //Vector3 pos = attachedCamera.ScreenToViewportPoint(Input.mousePosition - mouseOrigin);
-			/* Vector3 pos = transform.localPosition;
-			pos.x += horizontalPanning * 0.001f;
-			pos.y += verticalPanning * 0.001f;
-
-            Vector3 move = new Vector3(pos.x * panSpeed, pos.y * panSpeed, 0);
-            transform.Translate(move, Space.Self); */
-
-			float translateY = verticalPanning * panSpeed;
-			float translateX = horizontalPanning * panSpeed;
-            transform.Translate(translateX, translateY, 0, Space.Self);
-		}
 	}
 
     void ResetCamera() {
@@ -148,14 +98,4 @@ public class MoveCamera : MonoBehaviour
         transform.localRotation = defaulRotation;
 		attachedCamera.fieldOfView = defaultFOV;
     }
-
-	void DoZoom(float zoomFactor) {
-		/*Vector3 pos = attachedCamera.ScreenToViewportPoint(Input.mousePosition - mouseOrigin);
-
-		Vector3 move = pos.y * ( zoomSpeed * zoomFactor ) * transform.forward; 
-		transform.Translate(move, Space.Self);*/
-		float zoomTarget = attachedCamera.fieldOfView + zoomFactor;
-
-		attachedCamera.fieldOfView = Mathf.Clamp(zoomTarget, 10, 50);
-	}
 }
