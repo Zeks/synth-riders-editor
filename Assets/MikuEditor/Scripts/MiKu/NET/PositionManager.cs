194932a3a42e8c6044f6a355dd2538c602a2b2ef
zekses <enmarantispam@gmail.com>
2019-10-09 09:49:42 +0300

    unity version bump

---
+++ Assets/MikuEditor/Scripts/MiKu/NET/PositionManager.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading;
+using DG.Tweening;
+using DSPLib;
+using MiKu.NET.Charting;
+using Shogoki.Utils;
+using ThirdParty.Custom;
+using TMPro;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+using System.Diagnostics;
+
+namespace MiKu.NET {
+    
+
+
+    public static class PositionManager {
+
+    }
+}
